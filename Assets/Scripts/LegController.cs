d40fb37cca6ac3cfe527d5b0e9befdc7c55d4519
juzdepeche <qcidiot@gmail.com>
2020-06-06 12:26:12 -0400

    Move towards target when the distance between the leg and the target is too big

--- Assets/Scripts/LegController.cs
+++ Assets/Scripts/LegController.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class LegController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Transform legTarget;
+
+    [SerializeField]
+    private float distanceTreshold = 1.5f;
+    [SerializeField]
+    private float speed = 5f;
+    private float distance = 0;
+    private bool distanceTresholdExceeded = false;
+    private bool firstHalfOver = false;
+
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         RaycastHit hit;
         if (Physics.Raycast(transform.position, -Vector3.up, out hit)) {
@@ -20,6 +30,30 @@ public class LegController : MonoBehaviour
             {
                 Debug.DrawLine (transform.position, hit.point, Color.cyan);
                 transform.position = hit.point + new Vector3(0, 0.05f, 0);
+            }
+        }
+
+        distance = Vector3.Distance(transform.position, legTarget.position);
+
+        if (distance > distanceTreshold)
+        {
+            distanceTresholdExceeded = true;
+        }
+
+        //move the leg
+        if (distanceTresholdExceeded)
+        {
+            float step =  speed * Time.deltaTime; // calculate distance to move
+            var targetPosition = Vector3.MoveTowards(legTarget.position, transform.position, step);
+
+            //todo: add upwards vector for the first half
+
+            legTarget.position = targetPosition;
+
+            if (distance < 0.1f)
+            {
+                distanceTresholdExceeded = false;
+                firstHalfOver = false;
             }
         }
     }
