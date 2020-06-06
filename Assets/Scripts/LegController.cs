3190006872c8e37962232f0cc2ccc197c3559a1e
juzdepeche <qcidiot@gmail.com>
2020-06-06 11:30:40 -0400

    Raycast target point and adjust its position with the terrain

---
+++ Assets/Scripts/LegController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit)) {
+            // offsetDistance = hit.distance;
+            if (hit.transform.tag == "Walkable")
+            {
+                Debug.DrawLine (transform.position, hit.point, Color.cyan);
+                transform.position = hit.point + new Vector3(0, 0.05f, 0);
+            }
+        }
+    }
+}
